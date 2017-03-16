3508a7075c4150593aae127f7d70645f113f4ce8
tgisg <soniagupta504@gmail.com>
2017-03-16 11:08:57 -0600

    add suit assignment

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,19 +1,21 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-  @doc """
-  Hello world.
+    # for every element, assign to the word suit, and then pass into this block, and refer to this word suit (the element out of the array)
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
 
-  ## Examples
-
-      iex> Cards.hello
-      :world
-
-  """
-  def hello do
-    "hi there!"
   end
-  
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
 end
